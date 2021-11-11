a4760c3f71a0664ac7189363d5572c7f9dac2d62
Seibi0928 <nanteneus@hotmail.co.jp>
2021-11-11 23:13:01 +0900

    IAccountElementReader, IAccountElementWriterを継承したIAccountElementRepositoryを追加

---
+++ ResearchXBRL.Domain/AccountElements/IAccountElementRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResearchXBRL.Domain.AccountElements
+{
+    public interface IAccountElementRepository : IAccountElementReader, IAccountElementWriter
+    {
+    }
+}
