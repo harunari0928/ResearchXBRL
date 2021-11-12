e5df09ab5f8c060c07c3b27516efce80530c22a7
Seibi0928 <nanteneus@hotmail.co.jp>
2021-11-12 22:36:30 +0900

    IccountElementRepository追加

--- ResearchXBRL.Domain/AccountElements/IAccountElementRepository.cs
+++ ResearchXBRL.Domain/AccountElements/IAccountElementRepository.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ResearchXBRL.Domain.AccountElements
+﻿namespace ResearchXBRL.Domain.AccountElements
 {
     public interface IAccountElementRepository : IAccountElementReader, IAccountElementWriter
     {
