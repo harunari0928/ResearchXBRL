c6f39a6af334fa931e1c96e037dc8533260edbdd
e a <nanteneus@hotmail.co.jp>
2021-11-13 16:12:00 +0900

    テストコードの名前空間を修正

--- ResearchXBRL.Tests/CrossCuttingInterest/Extensions/XmlNodeExtensionsTests.cs
+++ ResearchXBRL.Tests/CrossCuttingInterest/Extensions/XmlNodeExtensionsTests.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Xml;
 using Xunit;
 
-namespace ResearchXBRL.Tests.ResearchXBRL.CrossCuttingInterest.Extensions
+namespace ResearchXBRL.Tests.CrossCuttingInterest.Extensions
 {
     public sealed class XmlNodeExtensionsTests
     {
