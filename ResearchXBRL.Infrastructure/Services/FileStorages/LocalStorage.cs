6694b188cfa4865767ef4572597d55183d45b3b5
Seibi0928 <nanteneus@hotmail.co.jp>
2021-11-16 02:13:46 +0900

    LocalStorageモジュールに対してのテストを実装

--- ResearchXBRL.Infrastructure/Services/FileStorages/LocalStorage.cs
+++ ResearchXBRL.Infrastructure/Services/FileStorages/LocalStorage.cs
@@ -1,4 +1,5 @@
 ﻿using ResearchXBRL.Application.Services;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
@@ -21,36 +22,91 @@ namespace ResearchXBRL.Infrastructure.Services.FileStorages
 
         public Stream Get(string filePath)
         {
+            if (IsDirectory(filePath))
+            {
+                throw new IOException("ファイルパスを指定してください");
+            }
+
             return new FileStream(CreateFullPath(filePath), FileMode.Open);
         }
 
         public void Set(in Stream inputStream, string filePath)
         {
+            var parentPath = Directory.GetParent(CreateFullPath(filePath))?.FullName
+                ?? throw new IOException("rootは指定できません");
+
+            if (IsDirectory(filePath))
+            {
+                throw new IOException("ファイルパスを指定してください");
+            }
+
+            if (!Directory.Exists(parentPath))
+            {
+                Directory.CreateDirectory(parentPath);
+            }
+
             using var localFileStream = new FileStream(
                 CreateFullPath(filePath),
                 FileMode.Create);
+            inputStream.Position = 0;
+            using var streamReader = new StreamReader(inputStream);
             using var writer = new StreamWriter(localFileStream);
-            writer.Write(inputStream);
+            writer.Write(streamReader.ReadToEnd());
             writer.Flush();
         }
 
         public IReadOnlyList<string> GetFiles(string directoryPath, string searchPattern = "*")
         {
-            return Directory.GetFiles(directoryPath, searchPattern).ToList();
+            if (!IsDirectory(directoryPath))
+            {
+                throw new IOException("ディレクトリパスを指定してください");
+            }
+
+            return Directory
+                .GetFiles(CreateFullPath(directoryPath), searchPattern)
+                .ToList();
         }
 
-        public void Unzip(string zipFilePath, string unzippedDirectoryPath, bool isDeleteOriginalZipFile)
+        public void Unzip(string zipFilePath, string unzippedDirectoryPath)
         {
+            if (IsDirectory(zipFilePath))
+            {
+                throw new IOException($"{nameof(zipFilePath)}には、ファイルパスを指定してください");
+            }
+
+            if (!IsDirectory(unzippedDirectoryPath))
+            {
+                throw new IOException($"{nameof(unzippedDirectoryPath)}には、ディレクトリパスを指定してください");
+            }
+
             ZipFile.ExtractToDirectory(
                 CreateFullPath(zipFilePath),
                 CreateFullPath(unzippedDirectoryPath));
-            if (isDeleteOriginalZipFile)
-            {
-                File.Delete(CreateFullPath(zipFilePath));
-            }
         }
 
         private string CreateFullPath(string path)
             => Path.Combine(storageDirectoryBasePath, path);
+
+        private static bool IsDirectory(string path)
+        {
+            path = path?.Trim() ?? throw new ArgumentNullException(nameof(path));
+
+            if (Directory.Exists(path))
+            {
+                return true;
+            }
+
+            if (File.Exists(path))
+            {
+                return false;
+            }
+
+            if (new[] { "\\", "/" }.Any(path.EndsWith))
+            {
+                return true;
+            }
+
+            return string.IsNullOrWhiteSpace(Path.GetExtension(path));
+        }
     }
 }
