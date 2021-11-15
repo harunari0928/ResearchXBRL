b1010c0b954949c0848b690e3922ddf6f268c366
Seibi0928 <nanteneus@hotmail.co.jp>
2021-11-16 00:40:59 +0900

    不要なusingを削除

--- ResearchXBRL.Tests/Infrastructure/Service/EdinetXBRLDownloaders/SecuritiesReportDownloaderTests.cs
+++ ResearchXBRL.Tests/Infrastructure/Service/EdinetXBRLDownloaders/SecuritiesReportDownloaderTests.cs
@@ -2,12 +2,9 @@
 using ResearchXBRL.Infrastructure.Services.EdinetXBRLDownloaders;
 using RichardSzalay.MockHttp;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Net.Http.Json;
-using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
