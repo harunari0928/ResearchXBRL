6ffa5f30933f924d71e8256241a7aa0ceba62564
Seibi0928 <nanteneus@hotmail.co.jp>
2021-11-20 02:17:34 +0900

    Interactorロジックに報告書永続化を実装

--- ResearchXBRL.Domain/FinancialReports/IFinancialReportRepository.cs
+++ ResearchXBRL.Domain/FinancialReports/IFinancialReportRepository.cs
@@ -1,6 +1,10 @@
-﻿namespace ResearchXBRL.Domain.FinancialReports
+﻿using System.Threading.Tasks;
+
+namespace ResearchXBRL.Domain.FinancialReports
 {
     public interface IFinancialReportRepository
     {
+        Task<bool> IsExists(FinancialReport reports);
+        Task Write(FinancialReport reports);
     }
 }
--- ResearchXBRL.Infrastructure/FinancialReports/FinancialReportRepository.cs
+++ ResearchXBRL.Infrastructure/FinancialReports/FinancialReportRepository.cs
@@ -1,8 +1,18 @@
 ﻿using ResearchXBRL.Domain.FinancialReports;
+using System.Threading.Tasks;
 
 namespace ResearchXBRL.Infrastructure.FinancialReports
 {
-    public sealed class FinancialReportRepository: IFinancialReportRepository
+    public sealed class FinancialReportRepository : IFinancialReportRepository
     {
+        public Task<bool> IsExists(FinancialReport reports)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task Write(FinancialReport reports)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
