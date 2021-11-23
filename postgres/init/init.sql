d6a47924e7a4ecb3f024a2e37208e3107c42d88d
Seibi0928 <nanteneus@hotmail.co.jp>
2021-11-23 10:43:59 +0000

    primarykeyの書き方修正

--- postgres/init/init.sql
+++ postgres/init/init.sql
@@ -8,30 +8,33 @@ create table report_covers (
 );
 
 create table units (
-  report_id uuid primary key,
-  unit_name varchar primary key,
+  report_id uuid,
+  unit_name varchar,
   unit_type integer not null,
   measure varchar null,
   unit_numerator varchar null,
-  unit_denominator  varchar null
+  unit_denominator  varchar null,
+  PRIMARY KEY (report_id, unit_name)
 );
 
 create table contexts (
-  report_id uuid primary key,
-  context_name varchar primary key,
+  report_id uuid,
+  context_name varchar,
   period_type integer not null,
   period_from date null,
   period_to date null,
-  instant_date date null
+  instant_date date null,
+  PRIMARY KEY (report_id, context_name)
 );
 
 create table report_items (
-  report_id uuid primary key,
-  classification varchar primary key,
-  xbrl_name varchar primary key,
+  report_id uuid,
+  classification varchar,
+  xbrl_name varchar,
   amounts decimal null,
   numerical_accuracy decimal null,
   scale decimal null,
   unit_name varchar not null,
-  context_name varchar not null
+  context_name varchar not null,
+  PRIMARY KEY (report_id, classification, xbrl_name)
 );
