81b239e009b4d043a61c928b138bfe551780e147
Seibi0928 <nanteneus@hotmail.co.jp>
2021-11-27 07:01:59 +0000

    企業マスタ情報インポートのためのcsproj作成

---
+++ Presentation/ImportCorporationInfo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ImportCorporationInfo
+{
+    sealed class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
