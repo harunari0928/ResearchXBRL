03da6f63b0c128d7693d333c3677dcf3525accf1
e a <nanteneus@hotmail.co.jp>
2021-11-28 10:16:34 +0000

    不要なプロジェクト削除

--- ResearchXBRL.Usecase/AccountElements/Transfer/ITransferAccountElementsUsecase.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace ResearchXBRL.Usecase.AccountElements.Transfer
-{
-    public interface ITransferAccountElementsUsecase
-    {
-        Task Hundle();
-    }
-}
