cdde126932ed880fa30816310d98606a4544a503
e a <nanteneus@hotmail.co.jp>
2021-11-28 10:08:26 +0000

    実行時引数から報告書取得期間を指定できるように修正

--- Presentation/AquireFinancialReports/Program.cs
+++ Presentation/AquireFinancialReports/Program.cs
@@ -9,6 +9,7 @@ using ResearchXBRL.Infrastructure.Services.EdinetXBRLDownloaders;
 using ResearchXBRL.Infrastructure.Services.EdinetXBRLParser;
 using ResearchXBRL.Infrastructure.Services.FileStorages;
 using System;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -16,12 +17,42 @@ namespace AquireFinancialReports
 {
     class Program
     {
-        static async Task Main(string[] _)
+        static async Task Main(string[] args)
         {
             using var serviceProvider = CreateServiceProvider();
             var usecase = serviceProvider
                 .GetService<IAquireFinancialReporsUsecase>();
-            await usecase.Handle(DateTimeOffset.Now.AddDays(-1), DateTimeOffset.Now);
+
+            var (from, to) = GetAquireSpan(args);
+
+            await usecase.Handle(from, to);
+        }
+
+        private static (DateTimeOffset, DateTimeOffset) GetAquireSpan(in string[] args)
+        {
+            // デフォルトでは直近1日の報告書を取得する
+            var from = DateTimeOffset.Now.AddDays(-1);
+            var to = DateTimeOffset.Now;
+            var fromArg = "--from";
+            if (args.Contains(fromArg))
+            {
+                from = new DateTimeOffset(GetDateTimeArg(args, fromArg), TimeSpan.FromHours(9));
+            }
+            var toArg = "--to";
+            if (args.Contains(toArg))
+            {
+                to = new DateTimeOffset(GetDateTimeArg(args, toArg), TimeSpan.FromHours(9));
+            }
+            return (from, to);
+        }
+
+        private static DateTime GetDateTimeArg(in string[] args, string argKey)
+        {
+            if (DateTime.TryParse(args.SkipWhile(x => x == argKey).FirstOrDefault(), out var from))
+            {
+                return from;
+            }
+            throw new ArgumentException($"{nameof(argKey)}には、日付を指定してください");
         }
 
         private static ServiceProvider CreateServiceProvider()
