9348ebc26ddf57c92d8fb69a480a656608b21719
Seibi0928 <ryo13579978@gmail.com>
2022-01-16 15:32:46 +0000

    勘定項目サジェスト機能での不要な部分を削除

--- ResearchXBRL.Domain/FinancialAnalysis/AnalysisMenus/AccountItemMenus/AccountItem.cs
+++ ResearchXBRL.Domain/FinancialAnalysis/AnalysisMenus/AccountItemMenus/AccountItem.cs
@@ -5,6 +5,5 @@ namespace ResearchXBRL.Domain.FinancialAnalysis.AnalysisMenus.AccountItemMenus
     public sealed class AccountItem
     {
         public string Name { get; init; } = "";
-        public IReadOnlyList<string> XBRLNames { get; init; } = new string[0];
     }
 }
