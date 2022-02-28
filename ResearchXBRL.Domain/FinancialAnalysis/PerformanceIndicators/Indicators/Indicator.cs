c806b07d99b7c581a5023ae197a0f52261c9033f
Seibi0928 <ryo13579978@gmail.com>
2022-02-28 03:45:12 +0000

    業績指標ドメインモデル

---
+++ ResearchXBRL.Domain/FinancialAnalysis/PerformanceIndicators/Indicators/Indicator.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+
+namespace ResearchXBRL.Domain.FinancialAnalysis.PerformanceIndicators.Indicators
+{
+    public sealed class Indicator
+    {
+        public IndicatorType IndicatorType { get; init; }
+
+        public IReadOnlyDictionary<DateOnly, decimal> Values { get; init; } = new Dictionary<DateOnly, decimal>();
+    }
+
+    public enum IndicatorType
+    {
+
+        /// <summary>
+        /// 売上高
+        /// </summary>
+        NetSales = 0,
+
+        /// <summary>
+        /// 営業利益
+        /// </summary>
+        OperatingIncome = 1,
+
+        /// <summary>
+        /// 経常利益
+        /// </summary>
+        OrdinaryIncome = 2,
+
+        /// <summary>
+        /// 親会社の所有者に帰属する利益
+        /// </summary>
+        ProfitLossAttributableToOwnersOfParent = 3,
+
+        /// <summary>
+        /// ROE
+        /// </summary>
+        RateOfReturnOnEquitySummaryOfBusinessResults = 4,
+
+        /// <summary>
+        /// １株当たり配当
+        /// </summary>
+        DividendPaidPerShareSummaryOfBusinessResults = 5
+    }
+}
