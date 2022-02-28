94d3e2335276f141db74d634d6fb7f87732ee7f0
Seibi0928 <ryo13579978@gmail.com>
2022-02-28 03:47:59 +0000

    リポジトリのインタフェース

---
+++ ResearchXBRL.Domain/FinancialAnalysis/PerformanceIndicators/IPerformanceIndicatorsRepository.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Threading.Tasks;
+
+namespace ResearchXBRL.Domain.FinancialAnalysis.PerformanceIndicators
+{
+    public interface IPerformanceIndicatorsRepository
+    {
+        ValueTask<PerformanceIndicators> Get(string corporationId);
+    }
+}
