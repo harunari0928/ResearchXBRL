5b5025928e5be73679fe01ff020b28be72bd474f
Seibi0928 <ryo13579978@gmail.com>
2022-03-08 05:10:06 +0000

    4桁証券コードで企業を検索できるように修正

--- ResearchXBRL.Domain/FinancialAnalysis/AnalysisMenus/Corporations/CorporatonMenu.cs
+++ ResearchXBRL.Domain/FinancialAnalysis/AnalysisMenus/Corporations/CorporatonMenu.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 
 namespace ResearchXBRL.Domain.FinancialAnalysis.AnalysisMenus.Corporations;
 
-public sealed class CorporatonMenu
+public sealed class CorporatonsMenu
 {
     public IReadOnlyList<Corporation> Corporations { get; init; } = new Corporation[0];
 }
