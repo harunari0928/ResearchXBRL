35e3d49d4b2a203e53709611518420e34d222096
Seibi0928 <ryo13579978@gmail.com>
2022-04-03 16:36:55 +0000

    ファイル取得、設置時に拡張子を見て例外を出す処理を廃止

--- ResearchXBRL.Infrastructure/Services/FileStorages/LocalFileStorage.cs
+++ ResearchXBRL.Infrastructure/Services/FileStorages/LocalFileStorage.cs
@@ -4,130 +4,125 @@ using System.IO;
 using System.IO.Compression;
 using System.Linq;
 
-namespace ResearchXBRL.Infrastructure.Services.FileStorages
+namespace ResearchXBRL.Infrastructure.Services.FileStorages;
+
+public sealed class LocalFileStorage : IFileStorage
 {
-    public sealed class LocalFileStorage : IFileStorage
+    private readonly string storageDirectoryBasePath;
+
+    public LocalFileStorage(string storageDirectoryBasePath)
     {
-        private readonly string storageDirectoryBasePath;
-
-        public LocalFileStorage(string storageDirectoryBasePath)
+        if (!Directory.Exists(storageDirectoryBasePath))
         {
-            if (!Directory.Exists(storageDirectoryBasePath))
-            {
-                Directory.CreateDirectory(storageDirectoryBasePath);
-            }
-            this.storageDirectoryBasePath = storageDirectoryBasePath;
+            Directory.CreateDirectory(storageDirectoryBasePath);
+        }
+        this.storageDirectoryBasePath = storageDirectoryBasePath;
+    }
+
+    public Stream? Get(in string filePath)
+    {
+        var fullFilePath = CreateFullPath(filePath);
+        if (!File.Exists(fullFilePath))
+        {
+            return null;
         }
 
-        public Stream? Get(string filePath)
+        return new FileStream(fullFilePath, FileMode.Open);
+    }
+
+    public void Set(in Stream inputStream, in string filePath)
+    {
+        var parentPath = Directory.GetParent(CreateFullPath(filePath))?.FullName;
+
+        if (parentPath is null)
         {
-            if (IFileStorage.IsDirectory(filePath))
-            {
-                throw new IOException($"{nameof(filePath)}には、ファイルパスを指定してください");
-            }
-
-            var fullFilePath = CreateFullPath(filePath);
-            if (!File.Exists(fullFilePath))
-            {
-                return null;
-            }
-
-            return new FileStream(fullFilePath, FileMode.Open);
+            throw new IOException($"{nameof(filePath)}には、ファイルパスを指定してください");
         }
 
-        public void Set(in Stream inputStream, string filePath)
+        if (!Directory.Exists(parentPath))
         {
-            var parentPath = Directory.GetParent(CreateFullPath(filePath))?.FullName;
-
-            if (parentPath is null || IFileStorage.IsDirectory(filePath))
-            {
-                throw new IOException($"{nameof(filePath)}には、ファイルパスを指定してください");
-            }
-
-            if (!Directory.Exists(parentPath))
-            {
-                Directory.CreateDirectory(parentPath);
-            }
-
-            using var localFileStream = new FileStream(
-                CreateFullPath(filePath),
-                FileMode.Create);
-            inputStream.Position = 0;
-            inputStream.CopyTo(localFileStream);
+            Directory.CreateDirectory(parentPath);
         }
 
-        public IReadOnlyList<string> GetFiles(string directoryPath, string searchPattern = "*")
-        {
-            if (!IFileStorage.IsDirectory(directoryPath))
-            {
-                throw new IOException($"{nameof(directoryPath)}には、ディレクトリパスを指定してください");
-            }
+        using var localFileStream = new FileStream(
+            CreateFullPath(filePath),
+            FileMode.Create);
+        inputStream.Position = 0;
+        inputStream.CopyTo(localFileStream);
+    }
 
-            return Directory
-                .GetFiles(CreateFullPath(directoryPath), searchPattern)
-                .Select(x => $"{string.Concat(x.Skip(storageDirectoryBasePath.Length))}")
-                .Select(x => $"./{x.Replace("./", "")}")
-                .Select(x => x.Replace("//", "/"))
-                .ToList();
+    public IReadOnlyList<string> GetFiles(string directoryPath, string searchPattern = "*")
+    {
+        if (!IFileStorage.IsDirectory(directoryPath))
+        {
+            throw new IOException($"{nameof(directoryPath)}には、ディレクトリパスを指定してください");
         }
 
-        public void Unzip(string zipFilePath, string unzippedDirectoryPath)
+        return Directory
+            .GetFiles(CreateFullPath(directoryPath), searchPattern)
+            .Select(x => $"{string.Concat(x.Skip(storageDirectoryBasePath.Length))}")
+            .Select(x => $"./{x.Replace("./", "")}")
+            .Select(x => x.Replace("//", "/"))
+            .ToList();
+    }
+
+    public void Unzip(string zipFilePath, string unzippedDirectoryPath)
+    {
+        if (IFileStorage.IsDirectory(zipFilePath))
         {
-            if (IFileStorage.IsDirectory(zipFilePath))
-            {
-                throw new IOException($"{nameof(zipFilePath)}には、ファイルパスを指定してください");
-            }
-
-            if (!IFileStorage.IsDirectory(unzippedDirectoryPath))
-            {
-                throw new IOException($"{nameof(unzippedDirectoryPath)}には、ディレクトリパスを指定してください");
-            }
-
-            var tmp = CreateFullPath(zipFilePath);
-            ZipFile.ExtractToDirectory(
-                CreateFullPath(zipFilePath),
-                CreateFullPath(unzippedDirectoryPath));
+            throw new IOException($"{nameof(zipFilePath)}には、ファイルパスを指定してください");
         }
 
-        private string CreateFullPath(in string path)
+        if (!IFileStorage.IsDirectory(unzippedDirectoryPath))
         {
-            var copiedPath = path;
-            // pathの先頭が/だとpathの値がそのまま帰ってくるので/を削除する
-            if (path.Any() && path[0] == '/')
-            {
-                copiedPath = string.Concat(path.Skip(1));
-            }
-            return Path.Combine(storageDirectoryBasePath, copiedPath);
+            throw new IOException($"{nameof(unzippedDirectoryPath)}には、ディレクトリパスを指定してください");
         }
 
-        public void Delete(string path)
+        var tmp = CreateFullPath(zipFilePath);
+        ZipFile.ExtractToDirectory(
+            CreateFullPath(zipFilePath),
+            CreateFullPath(unzippedDirectoryPath));
+    }
+
+    private string CreateFullPath(in string path)
+    {
+        var copiedPath = path;
+        // pathの先頭が/だとpathの値がそのまま帰ってくるので/を削除する
+        if (path.Any() && path[0] == '/')
         {
-            if (IFileStorage.IsDirectory(path))
-            {
-                Directory.Delete(CreateFullPath(path), true);
-            }
-            else
-            {
-                var fullPath = CreateFullPath(path);
-                if (!File.Exists(fullPath))
-                {
-                    throw new FileNotFoundException(fullPath);
-                }
-                File.Delete(fullPath);
-            }
+            copiedPath = string.Concat(path.Skip(1));
         }
+        return Path.Combine(storageDirectoryBasePath, copiedPath);
+    }
 
-        public IReadOnlyList<string> GetDirectoryNames(string directoryPath, string searchPattern = "*")
+    public void Delete(string path)
+    {
+        if (IFileStorage.IsDirectory(path))
         {
-            if (!IFileStorage.IsDirectory(directoryPath))
+            Directory.Delete(CreateFullPath(path), true);
+        }
+        else
+        {
+            var fullPath = CreateFullPath(path);
+            if (!File.Exists(fullPath))
             {
-                throw new ArgumentException($"{nameof(directoryPath)}には、ディレクトリパスを指定してください");
+                throw new FileNotFoundException(fullPath);
             }
-
-            return Directory.GetDirectories(CreateFullPath(directoryPath), searchPattern)
-                .Select(Path.GetFileName)
-                .OfType<string>()
-                .ToArray();
+            File.Delete(fullPath);
         }
     }
+
+    public IReadOnlyList<string> GetDirectoryNames(string directoryPath, string searchPattern = "*")
+    {
+        if (!IFileStorage.IsDirectory(directoryPath))
+        {
+            throw new ArgumentException($"{nameof(directoryPath)}には、ディレクトリパスを指定してください");
+        }
+
+        return Directory.GetDirectories(CreateFullPath(directoryPath), searchPattern)
+            .Select(Path.GetFileName)
+            .OfType<string>()
+            .ToArray();
+    }
+}
 }
--- ResearchXBRL.Infrastructure/Services/IFileStorage.cs
+++ ResearchXBRL.Infrastructure/Services/IFileStorage.cs
@@ -7,32 +7,21 @@ namespace ResearchXBRL.Infrastructure.Services;
 
 public interface IFileStorage
 {
-    public Stream? Get(string filePath);
-    public void Set(in Stream inputStream, string filePath);
+    public Stream? Get(in string filePath);
+    public void Set(in Stream inputStream, in string filePath);
     IReadOnlyList<string> GetFiles(string directoryPath, string searchPattern = "*");
     IReadOnlyList<string> GetDirectoryNames(string directoryPath, string searchPattern = "*");
     public void Unzip(string zipFilePath, string unzippedDirectoryPath);
     public void Delete(string path);
-
-    protected static bool IsDirectory(string path)
+    protected static bool IsDirectory(in string path)
     {
-        path = path?.Trim() ?? throw new ArgumentNullException(nameof(path));
+        var trimedPath = path.Trim();
 
-        if (Directory.Exists(path))
+        if (new[] { "\\", "/" }.Any(trimedPath.EndsWith))
         {
             return true;
         }
 
-        if (File.Exists(path))
-        {
-            return false;
-        }
-
-        if (new[] { "\\", "/" }.Any(path.EndsWith))
-        {
-            return true;
-        }
-
-        return string.IsNullOrWhiteSpace(Path.GetExtension(path));
+        return string.IsNullOrWhiteSpace(Path.GetExtension(trimedPath));
     }
 }
